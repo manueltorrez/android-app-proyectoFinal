8a4033e21e632c831b9ac9a9adc967792003271b
Valiente8 <valiente.andres@yahoo.com>
2016-11-17 15:29:33 -0600

    "Interfaces Primera Iteración"

---
+++ app/src/main/java/com/gangofseven/labs/app/Agregar_Prestamos.java
@@ -0,0 +1,13 @@
+package com.gangofseven.labs.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Agregar_Prestamos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar__prestamos);
+    }
+}
---
+++ app/src/main/java/com/gangofseven/labs/app/Prestamos_Pendientes.java
@@ -0,0 +1,13 @@
+package com.gangofseven.labs.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Prestamos_Pendientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prestamos__pendientes);
+    }
+}
