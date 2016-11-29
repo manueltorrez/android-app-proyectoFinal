b4ef71942876a7db51ef2dd8378f9769ff383384
Manuel Torrez <mastervado98@gmail.com>
2016-11-28 18:49:52 -0600

    Agregado enl metodo para guardar un prestamo

--- app/src/main/java/com/gangofseven/labs/app/AgregarPrestamos.java
+++ app/src/main/java/com/gangofseven/labs/app/AgregarPrestamos.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -16,13 +17,28 @@ public class AgregarPrestamos extends AppCompatActivity {
 
 
     private DatePickerDialog.OnDateSetListener onDateSetListener;
+    private DatePickerDialog.OnDateSetListener onDateSetListener2;
+    private EditText titulo;
+    private EditText nombrePres;
+    private EditText monto;
+    private EditText interes;
+    private Calendar calendario;
+    private Calendar calendario2;
+    private Button guardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_prestamos);
+
+        titulo = (EditText) findViewById(R.id.eTitulo);
+        nombrePres = (EditText) findViewById(R.id.eNombre);
+        monto = (EditText) findViewById(R.id.eMonto);
+        interes = (EditText) findViewById(R.id.eInteres);
+
         Button presta = (Button) findViewById(R.id.bFechaPre);
         Button paga = (Button) findViewById(R.id.bFechaPa);
+        guardar = (Button) findViewById(R.id.button5);
 
         setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -31,6 +47,22 @@ public class AgregarPrestamos extends AppCompatActivity {
                 Toast.makeText(AgregarPrestamos.this,"La fecha seleccionada es: "+view.getDayOfMonth()+
                         " de "+view.getMonth()+" de "+view.getYear(),Toast.LENGTH_LONG)
                         .show();
+                calendario = Calendar.getInstance();
+                calendario.set(year, month, dayOfMonth);
+
+            }
+        });
+
+        setOnDateSetListener2(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year2, int month2, int dayOfMonth2) {
+
+                Toast.makeText(AgregarPrestamos.this,"La fecha seleccionada es: "+view.getDayOfMonth()+
+                        " de "+view.getMonth()+" de "+view.getYear(),Toast.LENGTH_LONG)
+                        .show();
+                calendario2 = Calendar.getInstance();
+                calendario2.set(year2, month2, dayOfMonth2);
+
             }
         });
 
@@ -39,11 +71,16 @@ public class AgregarPrestamos extends AppCompatActivity {
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
+        final Calendar c2 = Calendar.getInstance();
+        int year2 = c2.get(Calendar.YEAR);
+        int month2 = c2.get(Calendar.MONTH);
+        int day2 = c2.get(Calendar.DAY_OF_MONTH);
+
         // Create a new instance of DatePickerDialog and return it
         final DatePickerDialog pickerDialog = new DatePickerDialog(this, null, year, month, day);
         pickerDialog.setCancelable(true);
 
-        final DatePickerDialog pickerDialog2 = new DatePickerDialog(this, null, year, month, day);
+        final DatePickerDialog pickerDialog2 = new DatePickerDialog(this, null, year2, month2, day2);
         pickerDialog2.setCancelable(true);
 
         pickerDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString( android.R.string.cancel), new DialogInterface.OnClickListener() {
@@ -73,7 +110,7 @@ public class AgregarPrestamos extends AppCompatActivity {
 
         pickerDialog2.setButton(DialogInterface.BUTTON_POSITIVE, getString( android.R.string.ok), new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-                getOnDateSetListener().onDateSet(pickerDialog.getDatePicker(), pickerDialog.getDatePicker().getYear(), pickerDialog.getDatePicker().getMonth(), pickerDialog.getDatePicker().getDayOfMonth());
+                getOnDateSetListener2().onDateSet(pickerDialog2.getDatePicker(), pickerDialog2.getDatePicker().getYear(), pickerDialog2.getDatePicker().getMonth(), pickerDialog2.getDatePicker().getDayOfMonth());
 
             }
         });
@@ -85,15 +122,37 @@ public class AgregarPrestamos extends AppCompatActivity {
         });
 
 
+        //Guardar préstamo
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPrestamo();
+            }
+        });
 
+
+
+    }
+
+    public void addPrestamo(){
+        Prestamo p = new Prestamo(titulo.getText().toString(), nombrePres.getText().toString(), Float.parseFloat(monto.getText().toString()), Float.parseFloat(interes.getText().toString()),calendario.getTime(), calendario2.getTime());
+        p.save();
     }
 
     public DatePickerDialog.OnDateSetListener getOnDateSetListener() {
         return onDateSetListener;
     }
 
+    public DatePickerDialog.OnDateSetListener getOnDateSetListener2() {
+        return onDateSetListener2;
+    }
+
     public void setOnDateSetListener(DatePickerDialog.OnDateSetListener onDateSetListener) {
         this.onDateSetListener = onDateSetListener;
+    }
+
+    public void setOnDateSetListener2(DatePickerDialog.OnDateSetListener onDateSetListener2) {
+        this.onDateSetListener2 = onDateSetListener2;
     }
 
 
